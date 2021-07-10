320912d67d689961968929736ac314c962046b2b
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-07-09 23:37:19 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+
+  photos = [
+    {
+      url:'https://www.estudarfora.org.br/wp-content/webp-express/webp-images/uploads/2018/05/cursos-sobre-Star-Wars-1-768x512.jpg.webp',
+      description: 'Mestre Yoda'
+    },
+    {
+      url:'https://www.hojeemdia.com.br/polopoly_fs/1.736936.1566431176!/image/image.jpg_gen/derivatives/landscape_653/image.jpg',
+      description: 'R2D2'
+    }
+  ];
 }
