787bca6b4e3fdff87caf3339ed33d13ac8de9dea
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-07-12 19:08:26 -0300

    add httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Photo } from './types/Photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
-  photos = [
-    {
-      url:'https://www.estudarfora.org.br/wp-content/webp-express/webp-images/uploads/2018/05/cursos-sobre-Star-Wars-1-768x512.jpg.webp',
-      description: 'Mestre Yoda'
-    },
-    {
-      url:'https://www.hojeemdia.com.br/polopoly_fs/1.736936.1566431176!/image/image.jpg_gen/derivatives/landscape_653/image.jpg',
-      description: 'R2D2'
-    }
-  ];
+  photos: Photo[] = [];
+
+  constructor(http: HttpClient){
+
+    http
+      .get<Photo[]>('http://localhost:3000/flavio/photos')
+      .subscribe(photos => {
+        console.log(photos);
+        this.photos = photos})
+  }
 }
