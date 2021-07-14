44959566f75ba4e3a71adeebc21e998c6e0f5928
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-07-14 16:10:17 -0300

    add pipe filter-by-description

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,9 +5,16 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { CommonModule } from "@angular/common";
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { filterbyDescription } from "./photo-list/filter-by-description.pipe";
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent ],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    filterbyDescription
+  ],
   imports: [ HttpClientModule, CommonModule ],
 })
 
